56eb76b39385a90659a3772e4c252a9b9b97e77d
John Nonmemory <formatator@gmail.com>
2020-02-13 09:12:26 +0300

    Добавлено получение конфигурации сервера из переменных окружения

--- cmd/gameserver/main.go
+++ cmd/gameserver/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/BurntSushi/toml"
 	"github.com/JohnNON/gamewithnums/internal/app/gameserver"
@@ -28,6 +29,38 @@ func main() {
 
 	if os.Getenv("PORT") != "" {
 		config.BindAddr = ":" + os.Getenv("PORT")
+	}
+
+	if os.Getenv("LOG_LEVEL") != "" {
+		config.LogLevel = os.Getenv("LOG_LEVEL")
+	}
+
+	if os.Getenv("DATABASE_URL") != "" {
+		config.DatabaseURL = os.Getenv("DATABASE_URL")
+	}
+
+	if os.Getenv("DATABASE_DRIVER") != "" {
+		config.DatabaseDriver = os.Getenv("DATABASE_DRIVER")
+	}
+
+	if os.Getenv("DATABASE_URL") != "" {
+		config.DatabaseURL = os.Getenv("DATABASE_URL")
+	}
+
+	if os.Getenv("SESSION_KEY") != "" {
+		config.SessionKey = os.Getenv("SESSION_KEY")
+	}
+
+	if os.Getenv("SESSION_MAXAGE") != "" {
+		config.SessionMaxAge = 3600
+		maxage, err := strconv.Atoi(os.Getenv("SESSION_MAXAGE"))
+		if err == nil {
+			config.SessionMaxAge = maxage
+		}
+	}
+
+	if os.Getenv("CSRF_KEY") != "" {
+		config.CsrfKey = os.Getenv("CSRF_KEY")
 	}
 
 	if err := gameserver.Start(config); err != nil {
